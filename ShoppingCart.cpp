9f60bcd5b2f2f5301076805b92e464886e8cab17
cdeamon141 <167352063+cdeamon141@users.noreply.github.com>
2024-04-22 13:36:00 -0500

    Update ShoppingCart.cpp
    getters for customer name and date

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,25 @@
-/* Type your code here */
+
+
+
+      
+      string ShoppingCart::GetCustomerName() const{
+        return customerName;
+      }
+      string ShoppingCart::GetDate() const(
+    return currentDate;
+      }
+      
+      
+      
+      int GetNumItemsInCart();
+      double GetCostOfCart();
+      
+     
+   
+   private:
+      string customerName;
+      string currentDate;
+      vector<ItemToPurchase> cartItems;
+};
+
+#endif here */
