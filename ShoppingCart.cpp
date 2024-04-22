29e4b9fd590928ee2abbd666a94998558f851f6a
izunga <167342792+izunga@users.noreply.github.com>
2024-04-22 13:48:08 -0500

    Update ShoppingCart.cpp
    Added Contructors

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,8 @@
 #include "ShoppingCart.h"
+
+void ShoppingCart::ShoppingCart(){};
+
+void void ShoppingCart::ShoppingCart(string name, string date){
+  customerName = name;
+  currentDate = date; 
+}
