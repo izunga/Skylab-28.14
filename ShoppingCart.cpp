4fda56fa0ebb608a3bf8231aa3af0fa3fe6134b8
cdeamon141 <167352063+cdeamon141@users.noreply.github.com>
2024-04-23 17:15:19 -0500

    Update ShoppingCart.cpp
    added getters

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -51,4 +51,27 @@ void ShoppingCart::ModifyItem(ItemToPurchase item) {
         cout << "Item not found in cart. Nothing modified." << endl;
     }
 
+string ShoppingCart::GetCustomerName() const{
+        return customerName;
+      }
+      string ShoppingCart::GetDate() const(
+    return currentDate;
+      }
+      
+      int ShoppingCart::GetNumItemsInCart(){
+          int num = 0;
+            for(int i = 0; i < cartItems.size(); i++){
+                  num += cartItem[i].GetQuantity();
+                        }
+            return num;
+      }
+                  
 
+                  
+      double ShoppingCart::GetCostOfCart(){
+             double num = 0;
+            for(int i = 0; i < cartItems.size(); i++){
+                  num += cartItem[i].GetCost();
+                        }
+            return num;      
+      }
