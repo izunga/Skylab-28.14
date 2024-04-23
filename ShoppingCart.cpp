ecd61d639e9298f8e47568692375b216ee243c07
cdeamon141 <167352063+cdeamon141@users.noreply.github.com>
2024-04-23 17:09:20 -0500

    Update ShoppingCart.cpp
    added getter functions

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,8 +1,60 @@
+#include "ShoppingCart.h"
+//all the setters
 
 
+void ShoppingCart::ShoppingCart(){};
 
-      
-      string ShoppingCart::GetCustomerName() const{
+void void ShoppingCart::ShoppingCart(string name, string date){
+  customerName = name;
+  currentDate = date; 
+}
+
+
+//setters
+
+void ShoppingCart::AddItem(ItemToPurchase item) {
+  cartItems.push_back(item);
+}
+
+void ShoppingCart::RemoveItem(string name) {
+  bool found = false;
+  for (unsigned int i = 0; I <carItems.size(); i++) {
+    if (cartItems[i].GetName() == name) {
+      cartItems.erase(cartItems.begin() + i);
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+        cout << "Item not found in cart. Nothing removed." << endl;
+    }
+}
+
+void ShoppingCart::ModifyItem(ItemToPurchase item) {
+  bool found = false;
+    for (unsigned int i = 0; i < cartItems.size(); ++i) {
+        if (cartItems[i].GetName() == item.GetName()) {
+            if (item.GetDescription() != "none") {
+                cartItems[i].SetDescription(item.GetDescription());
+            }
+            if (item.GetPrice() != 0) {
+                cartItems[i].SetPrice(item.GetPrice());
+            }
+            if (item.GetQuantity() != 0) {
+                cartItems[i].SetQuantity(item.GetQuantity());
+            }
+            found = true;
+            break;
+        }
+    }
+  if (!found) {
+        cout << "Item not found in cart. Nothing modified." << endl;
+    }
+
+
+//getters
+
+string ShoppingCart::GetCustomerName() const{
         return customerName;
       }
       string ShoppingCart::GetDate() const(
@@ -27,4 +79,6 @@
             return num;      
       }
 
+      
+     
 
