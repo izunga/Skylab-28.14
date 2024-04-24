c48492ebb0019ad8c4146f07f896e20d89bbdace
JadenKierulff <167344460+JadenKierulff@users.noreply.github.com>
2024-04-24 13:11:55 -0500

    Update main.cpp
    Step 3 - ask user for name and date and output that

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-//git repositiry link
+//git repository link
 //team members
 
 #include <iostream>
@@ -7,6 +7,20 @@
 using namespace std;
 
 #include "ShoppingCart.h"
+
+string name;
+string date;
+
+cout << "Enter customer's name:" << endl;
+cin >> name;
+
+cout << "Enter today's date:" << endl;
+cin >> date;
+
+cout << "Customer name: " << name << endl;
+cout << "Today's date: " << date << endl;
+
+ShoppingCart(name, date);
 
 void PrintMenu() {
    /* Type your code here */
