7fa273e482a6b2d3906c81158bfa549222579604
cdeamon141 <167352063+cdeamon141@users.noreply.github.com>
2024-04-24 13:10:22 -0500

    Update ItemToPurchase.cpp
    just defined functions

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 #include "ItemToPurchase.h"
 
+ItemToPurchase();
+      ItemToPurchase(string name, string description, 
+                  int price, int quantity = 0);
+      
+      void SetName(string name);
+      void SetDescription(string description);
+      void SetPrice(int price);
+      void SetQuantity(int quantity);
